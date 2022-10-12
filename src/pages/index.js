f40af4838353a293379ecd036d741f3e264e3b85
JiangShuuu <globelex65@gmail.com>
2022-10-12 20:59:23 +0800

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,18 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import '../css/index.css'
+// import '../css/index.css'
 // import clsx from 'clsx';
-// import styles from './index.module.css';
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className='person-header'>
-      <article className='person-box'>
-        <img src='/img/photo.jpeg' className="person-img" />
-        <p className='title'>John Lu</p>
-        <p className='text'>技術分享, 持續學習, 筆記記錄, 全端開發</p>
+    <header className={styles.person_header}>
+      <article className={styles.person_box}>
+        <img src='/img/photo.jpeg' className={styles.person_img} />
+        <p className={styles.title}>John Lu</p>
+        <p className={styles.text}>技術分享, 持續學習, 筆記記錄, 全端開發</p>
         <Link
           className="button button--secondary button--lg"
           to="/cv">
